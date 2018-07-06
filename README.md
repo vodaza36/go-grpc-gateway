ef44d612e7b38f39186d0cdbf77a2bcfa38a4fcd
Thomas Hochbichler <thomas@hochbichler.at>
2018-07-06 09:00:17 +0200

    add http handler

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ A demo showing the basic gRPC Golang usage
 ## Links
 
 - gRPC Article: <https://medium.com/pantomath/how-we-use-grpc-to-build-a-client-server-system-in-go-dd20045fa1c2>
+- OpenSSL Essentials: Working with SSL Certificates, Private Keys and CSRs: <https://www.digitalocean.com/community/tutorials/openssl-essentials-working-with-ssl-certificates-private-keys-and-csrs>
+
+## Technology Stack
+
+- gRPC
+- SSL/TLS
+- Makefile
+- Client Authentication via gRPC
--- client/main.go
+++ client/main.go
@@ -6,10 +6,42 @@ import (
 
 	"github.com/vodaza36/go-grpc-gateway/api"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
+// Authentication holds the login/password
+type Authentication struct {
+	Login    string
+	Password string
+}
+
+// GetRequestMetadata gets the current request metadata
+func (a *Authentication) GetRequestMetadata(context.Context, ...string) (map[string]string, error) {
+	return map[string]string{
+		"login":    a.Login,
+		"password": a.Password,
+	}, nil
+}
+
+// RequireTransportSecurity indicates whether the credentials requires transport security
+func (a *Authentication) RequireTransportSecurity() bool {
+	return true
+}
+
 func main() {
-	conn, err := grpc.Dial(":7777", grpc.WithInsecure())
+	// Create the client TLS credentials
+	creds, err := credentials.NewClientTLSFromFile("cert/server.crt", "")
+	if err != nil {
+		log.Fatalf("could not load tls cert: %s", err)
+	}
+
+	// Setup the login/pass
+	auth := Authentication{
+		Login:    "john",
+		Password: "doe",
+	}
+
+	conn, err := grpc.Dial("go-grpc-gateway.hochbichler.at:7777", grpc.WithTransportCredentials(creds), grpc.WithPerRPCCredentials(&auth))
 
 	if err != nil {
 		log.Fatalf("Error creating conn: %v", err)
